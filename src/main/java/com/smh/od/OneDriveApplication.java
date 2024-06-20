a3768ce3c2feed4d5cd5457c5af62a8cb38b6f7f
Sunil <sunilhanumante1@gmail.com>
2024-06-20 19:50:10 +0530

    This is OneDrive Project

---
+++ src/main/java/com/smh/od/OneDriveApplication.java
@@ -0,0 +1,13 @@
+package com.smh.od;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smh/od/OneDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smh.od;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
